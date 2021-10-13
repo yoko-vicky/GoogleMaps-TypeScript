85f663e12311fd07008a66e0723a2f79e85b7031
yocosaka <sakayoco55@gmail.com>
2021-10-13 16:40:57 +0900

    Add User class, install faker and @types/faker, type definition file

---
+++ src/User.ts
@@ -0,0 +1,16 @@
+import faker from "faker";
+
+export class User {
+  name: string
+  location: {
+    lat: number
+    lng: number
+  }
+  constructor() {
+    this.name = faker.name.firstName()
+    this.location = {
+      lat: +faker.address.latitude(),
+      lng: +faker.address.longitude()
+    }
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hi')
+import { User } from "./User";
+
+
+const user = new User()
+console.log(user)
